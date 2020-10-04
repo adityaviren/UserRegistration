9dad28b28f64171f0ebf199edd1d6c102eb69f27
unknown <adityaviren@gmail.com>
2020-10-04 16:22:25 +0530

    UC1_first_name

--- src/com/cg/learnregex/LearnRegex.java
+++ src/com/cg/learnregex/LearnRegex.java
@@ -1,11 +1,14 @@
 package com.cg.learnregex;
 
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class LearnRegex {
 
 	public static void main(String[] args) {
-			System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}", "Aditya"));
+		Scanner sc=new Scanner(System.in);
+		System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}", sc.nextLine()));
+		sc.close();
 	}
 
 }
