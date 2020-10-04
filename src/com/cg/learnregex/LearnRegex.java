98e67e66076cd2aad590f8378af7b876833169d1
unknown <adityaviren@gmail.com>
2020-10-04 16:37:49 +0530

    added lastNameCheck

--- src/com/cg/learnregex/LearnRegex.java
+++ src/com/cg/learnregex/LearnRegex.java
@@ -9,12 +9,19 @@ public class LearnRegex {
 		return Pattern.matches("[A-Z]{1}[a-z]{2,}", f_name);
 		
 	}
+	public static boolean lastNameCheck(String l_name) {
+		return Pattern.matches("[A-Z]{1}[a-z]{2,}", l_name);
+		
+	}
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter first name:");
 		String f_name=sc.nextLine();
 		System.out.println(firstNameCheck(f_name));
+		System.out.println("Enter last name:");
+		String l_name=sc.nextLine();
+		System.out.println(firstNameCheck(l_name));
 		sc.close();
 	}
 
