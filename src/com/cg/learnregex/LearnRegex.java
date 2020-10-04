845e50f9261dd04f34e1f7a81d64fd68d809050c
unknown <adityaviren@gmail.com>
2020-10-04 16:35:22 +0530

    added firstNameCheck

--- src/com/cg/learnregex/LearnRegex.java
+++ src/com/cg/learnregex/LearnRegex.java
@@ -4,10 +4,17 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class LearnRegex {
+	
+	public static boolean firstNameCheck(String f_name) {
+		return Pattern.matches("[A-Z]{1}[a-z]{2,}", f_name);
+		
+	}
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}", sc.nextLine()));
+		System.out.println("Enter first name:");
+		String f_name=sc.nextLine();
+		System.out.println(firstNameCheck(f_name));
 		sc.close();
 	}
 
